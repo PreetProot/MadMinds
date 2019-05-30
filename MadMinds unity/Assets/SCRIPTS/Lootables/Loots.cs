f2cd0768399e9c46b74b3b09316529cbd025312a
PreetProot <41739104+PreetProot@users.noreply.github.com>
2019-05-31 07:43:59 +0800

    Drop Loot with rarity values

---
+++ MadMinds unity/Assets/SCRIPTS/Lootables/Loots.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loots : MonoBehaviour
+{
+    [System.Serializable]
+
+    public class DropCurrency
+    {
+        public string name;
+        public GameObject item;
+        public int dropRarity;
+    }
+
+    public List<DropCurrency> Lootable = new List<DropCurrency>();
+    public int dropChance;
+
+    public void calcLoot()
+    {
+        //dropchnce
+        int calcDropChance = Random.Range(0, 101);
+
+        if(calcDropChance > dropChance)
+        {
+            Debug.Log("no loot for me");
+            return;
+        }
+
+        if(calcDropChance <= dropChance)
+        {
+            //do in weight
+            int itemWeight = 0; //itwem is calculate in weigth
+            for(int i = 0; i< Lootable.Count; i++)
+            {
+                itemWeight += Lootable[i].dropRarity;
+            }
+            Debug.Log("itemWeight = " + itemWeight);
+
+            //random value
+            int randomValue = Random.Range(0, itemWeight); //80%
+            for(int j = 0; j< Lootable.Count; j++)
+            {
+                if(randomValue <= Lootable[j].dropRarity )
+                {
+                    Instantiate(Lootable[j].item,transform.position, Quaternion.identity);
+                    return;//break for loop
+                }
+                randomValue -= Lootable[j].dropRarity;
+            }
+        }
+    }
+}
+
+   /* // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
