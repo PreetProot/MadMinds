294291cedd7cd7d169e8020a49df94bdced2a1b2
PreetProot <41739104+PreetProot@users.noreply.github.com>
2019-05-31 08:27:50 +0800

    pickups function

---
+++ MadMinds unity/Assets/SCRIPTS/nonLivingObjects/PickUpItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            print("item picked up");
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
